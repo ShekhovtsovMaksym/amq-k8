8bac9b62f8b2391c8a0e8f1101a48eb2fb860a13
ShekhovtsovMaksym <maksym.shekhovtsov@gmail.com>
2019-07-08 07:45:00 +0300

    added transmitter/receiver

--- amq-receiver/src/main/java/maksym/shekhovtsov/amqreceiver/AmqReceiverApplication.java
+++ amq-receiver/src/main/java/maksym/shekhovtsov/amqreceiver/AmqReceiverApplication.java
@@ -9,5 +9,4 @@ public class AmqReceiverApplication {
     public static void main(String[] args) {
         SpringApplication.run(AmqReceiverApplication.class, args);
     }
-
 }
--- amq-transmitter/src/test/java/maksym/shekhovtsov/amqtransmitter/AmqTransmitterApplicationTests.java
+++ amq-transmitter/src/test/java/maksym/shekhovtsov/amqtransmitter/AmqTransmitterApplicationTests.java
@@ -2,6 +2,7 @@ package maksym.shekhovtsov.amqtransmitter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AmqTransmitterApplicationTests {
 
+    @Autowired
+    private Transmitter transmitter;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void sendMessage() {
+        transmitter.send("lololo");
     }
 
 }
